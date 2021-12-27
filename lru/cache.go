9b624830dc7712a93c1e1811abeaf287f05d739a
Squwid <bwhitelaw24@gmail.com>
2021-12-27 15:14:00 +0000

    node removal

--- lru/cache.go
+++ lru/cache.go
@@ -8,6 +8,7 @@ type Cache[T CacheConstraint] struct {
 	m map[string]*Node[T]
 
 	MaxSize int
+	size    int
 
 	Head *Node[T]
 	Tail *Node[T]
@@ -29,20 +30,37 @@ func NewCache[T CacheConstraint](size int) *Cache[T] {
 	}
 }
 
+// Size returns the current size of the cache
+func (c *Cache[T]) Size() int { return c.size }
+
 func (c *Cache[T]) Add(item T) {
+	id := item.ID()
 	// If key already exists in cache, overwrite value and move to front
-	n, ok := c.m[item.ID()]
+	n, ok := c.m[id]
 	if ok {
 		n.Item = item
 		c.moveNodeToFront(n)
 	} else {
-		if len(c.m) == c.MaxSize {
+		if c.Size() == c.MaxSize {
 			c.evictTail()
 		}
 		n = &Node[T]{Item: item}
-		c.m[item.ID()] = n
+		c.m[id] = n
 		c.insertToFront(n)
 	}
+}
+
+// Remove removes an item from the cache. It returns the object if it was removed,
+// otherwise nil will be returned
+func (c *Cache[T]) Remove(id string) *T {
+	n, ok := c.m[id]
+	if !ok {
+		return nil
+	}
+	c.removeNode(n)
+	c.removeFromMap(id)
+
+	return &n.Item
 }
 
 func (c *Cache[T]) Get(key string) *T {
@@ -72,17 +90,58 @@ func (c *Cache[T]) PeekTail() *T {
 }
 
 func (c *Cache[T]) evictTail() {
-	newtail := c.Tail.Prev
+	if c.Tail == nil {
+		return
+	}
 
-	newtail.Prev.Next = nil
-	c.Tail = newtail
+	n := c.Tail
+	c.removeNode(n)
+	c.removeFromMap(n.Item.ID())
 }
+
+// removeNode removes a node from the cache. removeNode does NOT remove
+// the item from the map
+func (c *Cache[T]) removeNode(node *Node[T]) {
+	if node == nil {
+		return
+	}
+
+	if c.Size() == 1 {
+		c.Head = nil
+		c.Tail = nil
+	} else if c.Size() == 2 {
+		if node == c.Head {
+			c.Head = c.Tail
+			c.Head.Prev = nil
+		} else {
+			c.Tail = c.Head
+			c.Tail.Next = nil
+		}
+	} else {
+		if node == c.Head {
+			c.Head = node.Next
+			c.Head.Prev = nil
+		} else if node == c.Tail {
+			c.Tail = node.Prev
+			c.Tail.Next = nil
+		} else {
+			node.Prev.Next = node.Next
+			node.Next.Prev = node.Prev
+		}
+	}
+
+	node.Prev = nil
+	node.Next = nil
+	c.size--
+}
+
+func (c *Cache[T]) removeFromMap(id string) { delete(c.m, id) }
 
 // insertToFront will insert a node to the front of the cache. NOTE: this does not
 // actually add the node to the cache via map. That will be done upstream. This function
 // expects the node to already to be added to c.m
 func (c *Cache[T]) insertToFront(node *Node[T]) {
-	if len(c.m) <= 1 {
+	if c.Size() == 0 {
 		c.Head = node
 		c.Tail = node
 	} else {
@@ -91,35 +150,26 @@ func (c *Cache[T]) insertToFront(node *Node[T]) {
 		c.Head = node
 	}
 
+	c.size++
 }
 
 // moveNodeToFront moves a node that already exists in the cache to the front
 func (c *Cache[T]) moveNodeToFront(node *Node[T]) {
-	// If previous does not exist then node must be in the front, or if the cache has only one element
-	if node.Prev == nil || len(c.m) <= 1 {
+	if node == nil || node.Prev == nil {
 		return
 	}
 
-	// Modify previous and next nodes
-	prev := node.Prev
-	next := node.Next
-	prev.Next = next
-	if c.Tail == node {
-		c.Tail = prev
-	} else {
-		next.Prev = prev
-	}
-
-	node.Next = c.Head
-	c.Head.Prev = node
-	c.Head = node
+	c.removeNode(node)
+	c.insertToFront(node)
 }
 
 // ForEach will run the given function for each item in the cache in order of the cache
-func (c *Cache[T]) ForEach(f func(item T)) {
+func (c *Cache[T]) ForEach(f func(item T, index int)) {
 	node := c.Head
+	var i = 0
 	for node != nil {
-		f(node.Item)
+		f(node.Item, i)
 		node = node.Next
+		i++
 	}
 }
--- lru/cache_test.go
+++ lru/cache_test.go
@@ -15,7 +15,7 @@ func (l TestLRU) ID() string {
 	return l.Value
 }
 
-func TestCache(t *testing.T) {
+func TestSmallCache(t *testing.T) {
 	cache := NewCache[TestLRU](4)
 
 	assert.Nil(t, cache.PeekHead())
@@ -24,17 +24,68 @@ func TestCache(t *testing.T) {
 	item1 := TestLRU{Value: "1"}
 	item2 := TestLRU{Value: "2"}
 
+	cache.ForEach(func(item TestLRU, index int) { fmt.Printf("%v ", item.Value) })
+	fmt.Println("")
+	assert.Equal(t, cache.Size(), 0)
+	assert.Equal(t, len(cache.m), 0)
 	cache.Add(item1)
+	assert.Equal(t, cache.Size(), 1)
+	assert.Equal(t, len(cache.m), 1)
 	cache.Add(item2)
+	assert.Equal(t, cache.Size(), 2)
+	assert.Equal(t, len(cache.m), 2)
 
 	assert.Equal(t, cache.PeekHead().Value, item2.Value)
 	assert.Equal(t, cache.PeekTail().Value, item1.Value)
-	cache.ForEach(func(item TestLRU) {
-		fmt.Printf("%v ", item.Value)
-	})
+	cache.ForEach(func(item TestLRU, index int) { fmt.Printf("%v ", item.Value) })
 	fmt.Println("")
 
+	cache.ForEach(func(item TestLRU, index int) { fmt.Printf("%v ", item.Value) })
+	fmt.Println("")
 	assert.Equal(t, cache.Get(item1.Value).Value, item1.Value) // Moves item1 to front
 	assert.Equal(t, cache.PeekHead().Value, item1.Value)
 	assert.Equal(t, cache.PeekTail().Value, item2.Value)
+	assert.Equal(t, len(cache.m), 2)
+
+	cache.ForEach(func(item TestLRU, index int) { fmt.Printf("%v ", item.Value) })
+	fmt.Println("")
+	item3 := TestLRU{Value: "3"}
+	cache.Add(item3)
+	assert.Equal(t, cache.Size(), 3)
+	assert.Equal(t, len(cache.m), 3)
+	assert.Equal(t, cache.PeekHead().Value, item3.Value)
+	assert.Nil(t, cache.Get("abc"))
+
+	cache.ForEach(func(item TestLRU, index int) { fmt.Printf("%v ", item.Value) })
+	fmt.Println("")
+	assert.Equal(t, cache.Get(item2.Value).Value, item2.Value)
+	assert.Equal(t, cache.PeekHead().Value, item2.Value)
+	assert.Equal(t, cache.Size(), 3)
+	assert.Equal(t, len(cache.m), 3)
+	assert.Equal(t, cache.PeekTail().Value, item1.Value)
+
+	cache.ForEach(func(item TestLRU, index int) { fmt.Printf("%v ", item.Value) })
+	fmt.Println("")
+	assert.Nil(t, cache.Remove("abc"))
+	assert.Equal(t, cache.Remove(item1.Value).Value, item1.Value)
+	assert.Equal(t, cache.PeekHead().Value, item2.Value)
+	assert.Equal(t, cache.PeekTail().Value, item3.Value)
+	assert.Equal(t, cache.Size(), 2)
+	assert.Equal(t, len(cache.m), 2)
+
+	cache.ForEach(func(item TestLRU, index int) { fmt.Printf("%v ", item.Value) })
+	fmt.Println("")
+	assert.Equal(t, cache.Remove(item2.Value).Value, item2.Value)
+	assert.Equal(t, cache.PeekHead().Value, item3.Value)
+	assert.Equal(t, cache.PeekTail().Value, item3.Value)
+	assert.Equal(t, cache.Size(), 1)
+	assert.Equal(t, len(cache.m), 1)
+
+	cache.ForEach(func(item TestLRU, index int) { fmt.Printf("%v ", item.Value) })
+	fmt.Println("")
+	assert.Equal(t, cache.Remove(item3.Value).Value, item3.Value)
+	assert.Nil(t, cache.PeekHead())
+	assert.Nil(t, cache.PeekTail())
+	assert.Equal(t, cache.Size(), 0)
+	assert.Equal(t, len(cache.m), 0)
 }
